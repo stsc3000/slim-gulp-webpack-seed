982b9af00a0cb92208294ef748f86ef9339ae10a
Steffen Schildknecht <steffen.schildknecht@zweitag.de>
2015-07-16 09:54:31 +0200

    Remove webpack dev server reference

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require("gulp");
 var gutil = require("gulp-util");
 var webpack = require("webpack");
-var WebpackDevServer = require("webpack-dev-server");
 var webpackConfig = require("./webpack.config.js");
 var connect = require("gulp-connect");
 var jasmineBrowser = require('gulp-jasmine-browser');
